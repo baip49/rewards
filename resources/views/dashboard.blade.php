38a7e7249800f48d46e7dd6e35ef966008b51524
Baip <byp3ngu1n@gmail.com>
2025-04-09 04:53:14 -0600

    Reorganizar el código en la vista del dashboard: mover la lógica de rol al inicio y mejorar la estructura del HTML

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,13 +1,21 @@
 <x-layouts.app :title="__('dashboard.dashboard')">
-    <div class="flex h-full w-full flex-1 flex-col gap-4 rounded-xl">
-        <div class="relative h-full flex-1 overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
-            <div class="flex h-full w-full items-center justify-center bg-gradient-to-r from-blue-500 to-purple-600 text-white">
-                <div class="text-center">
-                    <h1 class="text-4xl font-bold">
-                        ¡Bienvenido, [{{ ucfirst(auth()->user()->role) }}] {{ auth()->user()->name }}!
-                    </h1>
-                </div>
+
+   @php
+      $role = auth()->user()->role;
+   @endphp
+
+   <div class="flex h-full w-full flex-1 flex-col gap-4 rounded-xl">
+      <div class="relative h-full flex-1 overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
+         <div
+            class="flex h-full w-full items-center justify-center bg-gradient-to-r from-blue-500 to-purple-600 text-white">
+            <div class="text-center">
+               <h1 class="text-4xl font-bold flex items-center">
+                  Bienvenido, {{ auth()->user()->name }}<flux:badge variant="solid"
+                     color="{{ $role === __('user.admin') ? 'red' : 'yellow' }}" class="ms-2" inset="bottom">
+                     {{ ucfirst($role) }}</flux:badge>
+               </h1>
             </div>
-        </div>
-    </div>
+         </div>
+      </div>
+   </div>
 </x-layouts.app>
